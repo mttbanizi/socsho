0cf376a129c9a3fa90b838f4a9b92e985b72075c
mttbanizi <mttbanizi@gmail.com>
2024-01-04 01:39:49 +0330

    edit wait-for-postgres.sh

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -5,7 +5,10 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p 5432 -U "$POSTGRES_USER" -c '\q'; do
+# Extract the IP address of the "db" service
+db_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' db)
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$db_ip" -p 5432 -U "$POSTGRES_USER" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
