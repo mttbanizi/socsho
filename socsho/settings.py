5fa8bc2d277ba281671633a96e0fa49212440303
mttbanizi <mttbanizi@gmail.com>
2024-01-07 16:16:02 +0330

    add static root in settings

--- socsho/settings.py
+++ socsho/settings.py
@@ -183,8 +183,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+STATIC_ROOT = '/var/www/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
